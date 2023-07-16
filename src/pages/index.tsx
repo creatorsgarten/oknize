95b5c8aa9e470e1c576108b4f0049898625bedf2
chayapatr <chayapatr.arc@gmail.com>
2023-07-16 11:23:51 +0700

    feat: about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { Button } from "../components/ui/button";
 import Image from "next/image";
+import Navbar from "@/components/navbar/Navbar";
 
 const Feature = () => {
   return (
@@ -10,8 +11,8 @@ const Feature = () => {
       </div>
       <h6 className="text-lg">Lorem Ipsum Dolor.</h6>
       <p className="text-center text-gray-500">
-        Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page
-        and in the loop.
+        Whether you have a team of 2 or 200, our shared team inboxes keep
+        everyone on the same page and in the loop.
       </p>
     </div>
   );
@@ -31,7 +32,8 @@ const Landing = () => {
               <span className="font-semibold">oknize 👌🏻</span>
             </h1>
             <h4 className="text-lg sm:text-xl text-neutral-500 text-center">
-              แพลตฟอร์มและชุดเครื่องมือ ออกแบบโดยนักจัดอีเวนต์ เพื่อนักจัดอีเวนต์
+              แพลตฟอร์มและชุดเครื่องมือ ออกแบบโดยนักจัดอีเวนต์
+              เพื่อนักจัดอีเวนต์
             </h4>
           </div>
           <div>
@@ -64,8 +66,9 @@ const Landing = () => {
               เราจะช่วยให้การจัดอีเวนต์สะดวกขึ้นได้อย่างไร
             </h1>
             <h6 className="text-center text-gray-500 max-w-2xl">
-              Powerful, self-serve product and growth analytics to help you convert, engage, and
-              retain more users. Trusted by over 4,000 startups.
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more users. Trusted by over 4,000
+              startups.
             </h6>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-12 pt-8 pb-16">
@@ -78,12 +81,15 @@ const Landing = () => {
       </section>
       <section className="bg-gray-50 p-8 flex flex-col justify-center items-center py- lg:py-16 gap-6 lg:gap-8 border-y border-gray-200">
         <h1 className="text-2xl lg:text-4xl text-center max-w-3xl">
-          จัดอีเวนต์สนุก เพื่อนชอบ ลูกค้าชอบ ทุกระดับประทับใจ ไม่ต้องปวดหัวกับปัญหายิบย่อยวุ่นวาย 🔥
+          จัดอีเวนต์สนุก เพื่อนชอบ ลูกค้าชอบ ทุกระดับประทับใจ
+          ไม่ต้องปวดหัวกับปัญหายิบย่อยวุ่นวาย 🔥
         </h1>
         <div className="flex flex-col justify-center items-center">
           <div className="h-12 rounded-full aspect-square bg-purple-500"></div>
           <h6 className="text-base pt-4">สมสวัสดิ์ กิตประเสริฐกาล</h6>
-          <p className="text-gray-500 text-sm">นักออกแบบแฮคอะทอน, Pra Titan Company</p>
+          <p className="text-gray-500 text-sm">
+            นักออกแบบแฮคอะทอน, Pra Titan Company
+          </p>
         </div>
       </section>
       <section>
@@ -92,7 +98,9 @@ const Landing = () => {
             <h1 className="text-3xl lg:text-4xl text-center">
               เกิดมาเพื่ออีเวนต์ และคนจัดอีเวนต์เท่านั้น
             </h1>
-            <h3 className="text-gray-700 text-2xl">คุณเคยเจอปัญหานี้หรือไม่:</h3>
+            <h3 className="text-gray-700 text-2xl">
+              คุณเคยเจอปัญหานี้หรือไม่:
+            </h3>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-4 px-8 lg:px-0">
             <div className="flex flex-col justify-center items-center gap-3">
@@ -109,7 +117,8 @@ const Landing = () => {
                 🗄️
               </div>
               <h6 className="text-center text-gray-500 max-w-2xl">
-                ใช้ notion ทำรายละเอียดอีเวนต์​ แต่นานเข้าโครงสร้างข้อมูลก็บวมจนหาไม่เจอ
+                ใช้ notion ทำรายละเอียดอีเวนต์​
+                แต่นานเข้าโครงสร้างข้อมูลก็บวมจนหาไม่เจอ
               </h6>
             </div>
             <div className="flex flex-col justify-center items-center gap-3">
@@ -117,7 +126,8 @@ const Landing = () => {
                 📑
               </div>
               <h6 className="text-center text-gray-500 max-w-2xl">
-                ใช้ excel ทำข้อมูลสตาฟ แต่รูปแบบข้อมูล --- จนนำไปใช้ประโยชน์อื่น ๆ ต่อได้ยาก
+                ใช้ excel ทำข้อมูลสตาฟ แต่รูปแบบข้อมูล --- จนนำไปใช้ประโยชน์อื่น
+                ๆ ต่อได้ยาก
               </h6>
             </div>
             <div className="flex flex-col justify-center items-center gap-3">
@@ -125,8 +135,8 @@ const Landing = () => {
                 📱
               </div>
               <h6 className="text-center text-gray-500 max-w-2xl">
-                รัน operation วันงานด้วยกระดาษและ messaging app แต่ก็ out-of-sync
-                จนต้องนัดบรีฟออฟไลน์กันบ่อยครั้งจนเสียเวลา
+                รัน operation วันงานด้วยกระดาษและ messaging app แต่ก็
+                out-of-sync จนต้องนัดบรีฟออฟไลน์กันบ่อยครั้งจนเสียเวลา
               </h6>
             </div>
           </div>
@@ -140,6 +150,9 @@ const Landing = () => {
             <h3 className="text-gray-700 text-lg max-w-3xl text-center">
               👌🏻 okmize ออกแบบมาเพื่อคนจัดอีเวนต์และจะเป็นเช่นนี้ต่อไป
               เพื่อสร้างเครื่องมือที่เป็นมิตรต่อกลุ่มผู้จัดอีเวนต์มากที่สุด
+            </h3>
+            <h3 className="text-purple-600 text-lg max-w-3xl text-center pt-3">
+              <a href="/about">🙋🏻 เกี่ยวกับเรา &gt;</a>
             </h3>
           </div>
         </div>
