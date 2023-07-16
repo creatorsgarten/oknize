15dda8627cafdd17c1ebff5cf649580b57f335b1
chayapatr <chayapatr.arc@gmail.com>
2023-07-16 08:59:35 +0700

    feat: landing section

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <footer className="flex flex-col p-8 justify-center items-center gap-4">
+      <footer className="flex flex-col p-8 justify-center items-center gap-3">
         <div className="text-3xl">👌🏻 oknize</div>
         <div className="text-gray-500">
           © 2077 Untitled UI. All rights reserved.
