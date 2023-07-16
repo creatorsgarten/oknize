8979922533c9ff05ebb6b487763041fe026d7b60
betich <betich365@gmail.com>
2023-07-16 10:09:33 +0700

    add onesignal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,3 @@
-import Navbar from "@/components/navbar/Navbar";
-import { Toaster } from "react-hot-toast";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -30,6 +28,23 @@ export default function Document() {
 
         {/* manifest */}
         <link rel="manifest" href="/manifest.json" />
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+  OneSignal.init({
+    appId: "f6432bfd-08f2-4880-88e5-b057cf6e926d",
+    safari_web_id: "web.onesignal.auto.58b504fd-a471-4836-bd65-020899577e4e",
+    notifyButton: {
+      enable: true,
+    },
+  });
+})`,
+          }}
+        />
       </Head>
       <body>
         <Main />
