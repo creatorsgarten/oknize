1ac4ad345a0af36920e0910313f504a9fc91712d
betich <betich365@gmail.com>
2023-07-16 10:46:17 +0700

    restructure project

---
+++ src/pages/event/view.tsx
@@ -0,0 +1,3 @@
+export default function View() {
+  return <h1>i</h1>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,132 +1,138 @@
-import { Nunito } from "next/font/google";
+import { Button } from "../components/ui/button";
 
-const nunito = Nunito({ subsets: ["latin"] });
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { useEffect, useState } from "react";
-import { deleteTask, editTask, subscribeSchedule } from "@/lib/db";
-import { ScheduleSlot, getCurrentSlot, getNextSlot } from "@/lib/schedule";
-import CurrentTime from "@/components/time/CurrentTime";
-
-import TaskTable from "@/components/table/TaskTable";
-import ProgressBar from "@/components/time/ProgressBar";
-import runOneSignal from "@/lib/onesignal";
-
-export default function Home() {
-  const [ScheduleSlots, setScheduleSlots] = useState<ScheduleSlot[]>([]);
-  const [currentSlot, setCurrentSlot] = useState<ScheduleSlot | null>(null);
-  const [nextSlot, setNextSlot] = useState<ScheduleSlot | null>(null);
-  const [currentTime, setCurrentTime] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = subscribeSchedule(
-      "ywc19",
-      (data: { agenda: ScheduleSlot[] }) => {
-        setScheduleSlots(data.agenda);
-
-        setCurrentSlot(getCurrentSlot(data.agenda));
-        setNextSlot(getNextSlot(data.agenda));
-      }
-    );
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
-  const handleOnDeleteTaskData = (data: ScheduleSlot) => {
-    deleteTask("ywc19", ScheduleSlots, data);
-  };
-
-  const handleSaveChanges = (data: ScheduleSlot) => {
-    editTask("ywc19", ScheduleSlots, data);
-  };
-
-  useEffect(() => {
-    function updateInterval() {
-      setCurrentSlot(getCurrentSlot(ScheduleSlots));
-      setNextSlot(getNextSlot(ScheduleSlots));
-    }
-
-    const interval = setInterval(() => {
-      var today = new Date();
-      var now = today.toLocaleTimeString("th-TH");
-      setCurrentTime(now);
-
-      updateInterval();
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [ScheduleSlots]);
-
-  useEffect(() => {
-    runOneSignal();
-  }, []);
-
+const Feature = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center p-12 ${nunito.className}`}
-    >
-      <div className="grid grid-cols-1 md:grid-cols-5 items-center justify-between w-full gap-12">
-        <Card className="order-last md:col-span-3 md:order-first w-full">
-          <CardHeader>
-            <CardTitle>Young Webmaster Camp 19th (YWC19)</CardTitle>
-            <CardDescription>
-              ค่ายสร้างเว็บระดับอุดมศึกษาที่จัดโดยสมาคมผู้ดูแลเว็บและสื่อออนไลน์ไทย
-              จัดขึ้นเป็นประจำทุกปี
-              เพื่อผลิตบุคลากรหน้าใหม่ด้านดิจิทัลให้พร้อมเข้าสู่การทำงานด้านดิจิทัลในอนาคต
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Date: 14 - 17 July, 2023</p>
-            <p>Place: SCB Training Center</p>
-          </CardContent>
-        </Card>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <div className="bg-purple-100 text-4xl rounded-full aspect-square p-4 flex items-center justify-center">
+        K
+      </div>
+      <h6 className="text-lg">Lorem Ipsum Dolor.</h6>
+      <p className="text-center text-gray-500">
+        Whether you have a team of 2 or 200, our shared team inboxes keep
+        everyone on the same page and in the loop.
+      </p>
+    </div>
+  );
+};
 
-        <div className="order-first md:col-span-2 md:order-last flex flex-col gap-6 w-full items-center">
-          <h1 className="flex flex-row gap-2 items-baseline text-neutral-500">
-            Current Time
-          </h1>
-
-          <CurrentTime currentTime={currentTime} />
-
-          <ProgressBar currentTime={currentTime} currentSlot={currentSlot} />
-
-          <div className="flex flex-row justify-center gap-10 w-full text-sm">
-            <div className="flex flex-col">
-              <h2 className="font-bold text-black">กิจกรรมปัจจุบัน</h2>
-              <span className="text-neutral-500">
-                {currentSlot
-                  ? `${currentSlot.title} (${currentSlot.start}-${currentSlot.end})`
-                  : "No Slot"}
-              </span>
-            </div>
-            <div className="flex flex-col">
-              <h2 className="font-bold text-black">กิจกรรมถัดไป</h2>
-              <span className="text-neutral-500">
-                {nextSlot
-                  ? `${nextSlot.title} (${nextSlot.start}-${nextSlot.end})`
-                  : "No Slot"}
-              </span>
-            </div>
+const Landing = () => {
+  return (
+    <div>
+      <section>
+        <div className="p-10 flex flex-col gap-6 justify-center items-center sm:p-16">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl text-center">
+              จัดอีเวนต์ให้ <span className="font-semibold">nice</span> ด้วย{" "}
+              <span className="font-semibold">oknize 👌🏻</span>
+            </h1>
+            <h4 className="text-lg sm:text-xl text-neutral-500 text-center">
+              แพลตฟอร์มและชุดเครื่องมือ ออกแบบโดยนักจัดอีเวนต์
+              เพื่อนักจัดอีเวนต์
+            </h4>
+          </div>
+          <div>
+            <Button className="bg-purple-500">ลองใช้เลย!</Button>
           </div>
         </div>
-      </div>
-
-      <div className="mt-8 w-full">
-        <TaskTable
-          tableData={ScheduleSlots}
-          onSave={handleSaveChanges}
-          onDelete={handleOnDeleteTaskData}
-        />
-      </div>
-    </main>
+      </section>
+      <section className="py-4">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div className="px-10 lg:px-16 flex flex-col justify-center items-center gap-4">
+            <h4 className="text-lg text-purple-500 text-center">คุณสมบัติ</h4>
+            <h1 className="text-3xl lg:text-4xl text-center">
+              เราจะช่วยให้การจัดอีเวนต์สะดวกขึ้นได้อย่างไร
+            </h1>
+            <h6 className="text-center text-gray-500 max-w-2xl">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more users. Trusted by over 4,000
+              startups.
+            </h6>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-12 pt-8 pb-16">
+            <Feature />
+            <Feature />
+            <Feature />
+            <Feature />
+          </div>
+        </div>
+      </section>
+      <section className="bg-gray-50 p-8 flex flex-col justify-center items-center py- lg:py-16 gap-6 lg:gap-8 border-y border-gray-200">
+        <h1 className="text-2xl lg:text-4xl text-center max-w-3xl">
+          จัดอีเวนต์สนุก เพื่อนชอบ ลูกค้าชอบ ทุกระดับประทับใจ
+          ไม่ต้องปวดหัวกับปัญหายิบย่อยวุ่นวาย 🔥
+        </h1>
+        <div className="flex flex-col justify-center items-center">
+          <div className="h-12 rounded-full aspect-square bg-purple-500"></div>
+          <h6 className="text-base pt-4">สมสวัสดิ์ กิตประเสริฐกาล</h6>
+          <p className="text-gray-500 text-sm">
+            นักออกแบบแฮคอะทอน, Pra Titan Company
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="p-10 lg:px-16 lg:pt-24 flex flex-col justify-center items-center gap-8">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <h1 className="text-3xl lg:text-4xl text-center">
+              เกิดมาเพื่ออีเวนต์ และคนจัดอีเวนต์เท่านั้น
+            </h1>
+            <h3 className="text-gray-700 text-2xl">
+              คุณเคยเจอปัญหานี้หรือไม่:
+            </h3>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-4 px-8 lg:px-0">
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="bg-purple-100 rounded-full aspect-square text-4xl p-4 border border-purple-200 flex items-center justify-center">
+                📅
+              </div>
+              <h6 className="text-center text-gray-500 max-w-2xl">
+                ใช้ monday ทำ agenda
+                แล้วการแก้บล็อคเวลาสักช่วงทำให้ในหลายครั้งคุณต้องรื้อโครงสร้างของตารางใหม่
+              </h6>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="bg-purple-100 rounded-full aspect-square text-4xl p-4 border border-purple-200 flex items-center justify-center">
+                🗄️
+              </div>
+              <h6 className="text-center text-gray-500 max-w-2xl">
+                ใช้ notion ทำรายละเอียดอีเวนต์​
+                แต่นานเข้าโครงสร้างข้อมูลก็บวมจนหาไม่เจอ
+              </h6>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="bg-purple-100 text-4xl p-4 rounded-full aspect-square border border-purple-200 flex items-center justify-center">
+                📑
+              </div>
+              <h6 className="text-center text-gray-500 max-w-2xl">
+                ใช้ excel ทำข้อมูลสตาฟ แต่รูปแบบข้อมูล --- จนนำไปใช้ประโยชน์อื่น
+                ๆ ต่อได้ยาก
+              </h6>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="bg-purple-100 rounded-full aspect-square text-4xl p-4 border border-purple-200 flex items-center justify-center">
+                📱
+              </div>
+              <h6 className="text-center text-gray-500 max-w-2xl">
+                รัน operation วันงานด้วยกระดาษและ messaging app แต่ก็
+                out-of-sync จนต้องนัดบรีฟออฟไลน์กันบ่อยครั้งจนเสียเวลา
+              </h6>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4 lg:gap-2 max-w-2xl">
+            <h3 className="text-gray-700 text-lg max-w-3xl text-center">
+              เพราะเครื่องมือที่
+              <span className="font-semibold">ครอบคลุมที่สุด </span>
+              อาจไม่ใช่เครื่องมือที่
+              <span className="font-semibold">สะดวกที่สุด </span>เสมอไป
+            </h3>
+            <h3 className="text-gray-700 text-lg max-w-3xl text-center">
+              👌🏻 okmize ออกแบบมาเพื่อคนจัดอีเวนต์และจะเป็นเช่นนี้ต่อไป
+              เพื่อสร้างเครื่องมือที่เป็นมิตรต่อกลุ่มผู้จัดอีเวนต์มากที่สุด
+            </h3>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-}
+};
+
+export default Landing;
