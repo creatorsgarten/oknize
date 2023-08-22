62228f70173d8c6535162cb2c7d5a7528e45f3bd
betich <betich365@gmail.com>
2023-08-22 23:19:28 +0700

    chore: refactor useSearch

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useSearch<T>(data: T[], searchFn: (data: T[]) => T[]) {
     const [filteredData, setFilteredData] = useState<T[]>(data);
 
-    const handleSearch = (query: string) => {
-        const filteredData = searchFn(data);
-        setFilteredData(filteredData);
-    };
+    const handleSearch = useCallback(
+        (query: string) => {
+            const filteredData = searchFn(data);
+            setFilteredData(filteredData);
+        },
+        [data, searchFn]
+    );
 
     return { filteredData, handleSearch };
 }
