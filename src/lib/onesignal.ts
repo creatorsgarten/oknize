0cb91029e36dc96a79fab6d9f6dd11c69b5b0b82
betich <betich365@gmail.com>
2024-07-05 17:04:51 +0700

    feat: update onesignal

--- src/lib/onesignal.ts
+++ src/lib/onesignal.ts
@@ -1,9 +1,36 @@
 import OneSignal from 'react-onesignal';
 
-export default async function runOneSignal() {
+export async function initOneSignal() {
     await OneSignal.init({
         appId: 'f6432bfd-08f2-4880-88e5-b057cf6e926d',
         allowLocalhostAsSecureOrigin: true,
     });
-    OneSignal.showSlidedownPrompt();
+}
+
+export async function runOneSignal() {
+    // sendGTMEvent({
+    //   category: 'Notification',
+    //   action: 'subscribe',
+    //   label: 'subscribe',
+    // })
+
+    OneSignal.Slidedown.promptPush();
+
+    OneSignal.Notifications.addEventListener('click', (event) => {
+        // sendGTMEvent({
+        //   category: 'Notification',
+        //   action: 'click',
+        //   label: event.notification.title,
+        // })
+    });
+}
+
+export function isUserSubscribed() {
+    const subscriptionId = OneSignal.User.PushSubscription.id;
+
+    return !!subscriptionId;
+}
+
+export function subscribeToNotification() {
+    runOneSignal();
 }
